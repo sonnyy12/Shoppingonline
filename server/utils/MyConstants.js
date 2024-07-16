1534ea3c1233209e946a89a78619b00d47272f3e
sonnyy12 <145656025+sonnyy12@users.noreply.github.com>
2024-07-16 08:44:04 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster1.oyx86v2.mongodb.net',
+    DB_USER: 'sontran',
+    DB_PASS: '123456s',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'transon',
+    JWT_EXPIRES: '3600000000', // in milliseconds
+    EMAIL_USER: 'sonttt028@gmail.com', // gmail service
+    EMAIL_PASS: 'fgxy flld ierx etnt'
+  };
+  module.exports = MyConstants;
