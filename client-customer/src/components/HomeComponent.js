821dab48a5f096929e28f827567053753dfc3277
sonnyy12 <145656025+sonnyy12@users.noreply.github.com>
2024-07-16 09:14:38 +0700

    Update HomeComponent.js

--- client-customer/src/components/HomeComponent.js
+++ client-customer/src/components/HomeComponent.js
@@ -22,7 +22,7 @@ class Home extends Component {
     });
     const hotprods = this.state.hotprods.map((item) => {
       return (
-        <div key={item._id} className="inline">a 
+        <div key={item._id} className="inline">
           <figure>
             <Link to={'/product/' + item._id}><img src={"data:image/jpg;base64," + item.image} width="300px" height="300px" alt="" /></Link>
             <figcaption className="text-center">{item.name}<br />Price: {item.price}</figcaption>
